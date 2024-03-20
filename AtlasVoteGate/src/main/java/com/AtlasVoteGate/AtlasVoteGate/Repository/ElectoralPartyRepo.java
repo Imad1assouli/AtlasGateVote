3bca300e8b66bc27ed082ea91af8d6f6e79b0f30
Mossab <mosaablachhab06@gmail.com>
2024-03-20 23:39:44 +0000

    ElectoralParty service update

--- AtlasVoteGate/src/main/java/com/AtlasVoteGate/AtlasVoteGate/Repository/ElectoralPartyRepo.java
+++ AtlasVoteGate/src/main/java/com/AtlasVoteGate/AtlasVoteGate/Repository/ElectoralPartyRepo.java
@@ -4,6 +4,9 @@ import com.AtlasVoteGate.AtlasVoteGate.model.ElectoralParty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ElectoralPartyRepo extends JpaRepository<ElectoralParty,Long> {
+    Optional<ElectoralParty> findByName (String name);
 }
